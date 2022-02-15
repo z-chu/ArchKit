746c20458981dbc398e8b8455ec63ce537884f43
赵成柱(chengzhu.zhao) <chengzhu.zhao@huolala.cn>
2022-02-15 11:10:22 +0800

    feat: update dependencies , kotlin 1.5.31

--- koin-auto-service/src/main/java/com/github/zchu/arch/koin/auto/service/KoinAutoInstall.kt
+++ koin-auto-service/src/main/java/com/github/zchu/arch/koin/auto/service/KoinAutoInstall.kt
@@ -4,15 +4,13 @@ import org.koin.core.KoinApplication
 import org.koin.core.logger.Level
 import org.koin.core.module.Module
 import java.util.*
-import kotlin.collections.HashMap
 
 fun KoinApplication.installAutoRegister() {
     val installableServiceLoader = ServiceLoader.load(KoinAutoInstallable::class.java)
     val modules = ArrayList<Module>(20)
     val properties = HashMap<String, String>()
     var installables: ArrayList<KoinAutoInstallable>? = null
-
-    if (koin._logger.isAt(Level.DEBUG)) {
+    if (koin.logger.isAt(Level.DEBUG)) {
         installables = ArrayList<KoinAutoInstallable>(20)
     }
 
@@ -22,15 +20,15 @@ fun KoinApplication.installAutoRegister() {
         for (property in it.properties) {
             val value = properties[property.key]
             if (value != null) {
-                if (koin._logger.isAt(Level.ERROR)) {
-                    koin._logger.error("A property with the same key appears, please modify the key to prevent conflicts。key=${property.key},value1=${value}]，value2=${value}]。")
+                if (koin.logger.isAt(Level.ERROR)) {
+                    koin.logger.error("A property with the same key appears, please modify the key to prevent conflicts。key=${property.key},value1=${value}]，value2=${value}]。")
                 }
             }
             properties[property.key] = property.value
         }
     }
     if (installables != null) {
-        koin._logger.debug("Koin auto install :${installables.map { it.javaClass.name }}")
+        koin.logger.debug("Koin auto install :${installables.map { it.javaClass.name }}")
     }
     modules(modules)
     properties(properties)
